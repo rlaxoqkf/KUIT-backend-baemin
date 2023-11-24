c3fde0877d68898d769c4a21d82c82b199c6f7d2
rlaxoqkf <rlaxoqkf96@gmail.com>
2023-11-24 22:45:48 +0900

    코드 업로드

---
+++ src/main/java/kuit/server/dto/user/PostUserRequest.java
@@ -0,0 +1,56 @@
+package kuit.server.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostUserRequest {
+
+    @NotBlank(message = "name: {NotBlank}")
+    @Length(max = 20, message = "name: 최대 {max}자리까지 가능합니다")
+    private String name;
+
+    @Nullable
+    @Length(max = 20, message = "nickname: 최대 {max}자리까지 가능합니다")
+    private String nickname;
+
+    @Nullable
+    @Length(max = 500, message = "profileImage: 최대 {max}자리까지 가능합니다")
+    private String profileImage;
+
+    @NotBlank(message = "phoneNumber: {NotBlank}")
+    @Length(max = 30, message = "phoneNumber: 최대 {max}자리까지 가능합니다")
+    private String phoneNumber;
+
+    @Email(message = "email: 이메일 형식이어야 합니다")
+    @NotBlank(message = "email: {NotBlank}")
+    @Length(max = 30, message = "email: 최대 {max}자리까지 가능합니다")
+    private String email;
+
+    private String grade;
+
+    @NotBlank(message = "password: {NotBlank}")
+    @Length(min = 8, max = 20,
+            message = "password: 최소 {min}자리 ~ 최대 {max}자리까지 가능합니다")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}",
+            message = "password: 대문자, 소문자, 특수문자가 적어도 하나씩은 있어야 합니다")
+    private String password;
+
+    @Nullable
+    private Double latitude;
+
+    @Nullable
+    private Double longitude;
+
+    public void resetPassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+}
